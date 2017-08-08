fbdfdf0cb8728fa1fdc096e2e6e4c8aea1c9b657
out-of-my-mind <380687541@qq.com>
2017-08-08 20:57:11 +0800

    Initial commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnselectfolder = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.GridViewFileShow = new System.Windows.Forms.DataGridView();
+            this.checkb = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.historyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.GridViewFileShow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "选择要修改css/js链接页面所在文件夹";
+            // 
+            // btnselectfolder
+            // 
+            this.btnselectfolder.Location = new System.Drawing.Point(245, 28);
+            this.btnselectfolder.Name = "btnselectfolder";
+            this.btnselectfolder.Size = new System.Drawing.Size(98, 23);
+            this.btnselectfolder.TabIndex = 0;
+            this.btnselectfolder.Text = "选择文件夹";
+            this.btnselectfolder.UseVisualStyleBackColor = true;
+            this.btnselectfolder.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // GridViewFileShow
+            // 
+            this.GridViewFileShow.AllowUserToOrderColumns = true;
+            this.GridViewFileShow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.GridViewFileShow.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.GridViewFileShow.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridViewFileShow.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.checkb});
+            this.GridViewFileShow.Location = new System.Drawing.Point(12, 57);
+            this.GridViewFileShow.Name = "GridViewFileShow";
+            this.GridViewFileShow.RowTemplate.Height = 23;
+            this.GridViewFileShow.Size = new System.Drawing.Size(668, 142);
+            this.GridViewFileShow.TabIndex = 2;
+            // 
+            // checkb
+            // 
+            this.checkb.HeaderText = "";
+            this.checkb.MinimumWidth = 30;
+            this.checkb.Name = "checkb";
+            this.checkb.Width = 30;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(545, 211);
+            this.trackBar1.Minimum = 2;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar1.Size = new System.Drawing.Size(128, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 2;
+            this.trackBar1.Visible = false;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(372, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(513, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "分析文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(278, 212);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(119, 23);
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(605, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "处理文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(703, 25);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.historyToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(44, 21);
+            this.toolStripMenuItem1.Text = "菜单";
+            // 
+            // historyToolStripMenuItem
+            // 
+            this.historyToolStripMenuItem.Name = "historyToolStripMenuItem";
+            this.historyToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.historyToolStripMenuItem.Text = "历史记录";
+            this.historyToolStripMenuItem.Click += new System.EventHandler(this.historyToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(703, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GridViewFileShow);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnselectfolder);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "更新页面css/js链接应用程序";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GridViewFileShow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnselectfolder;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView GridViewFileShow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn checkb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem historyToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class filemodel {
+            public string path { set;get;}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //选择文件夹
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = folderBrowserDialog1.SelectedPath;
+                textBox1.Text = path;
+                BindData("*.aspx");
+            }
+            //Calendar cc = new Calendar(DateTime.Now);
+            //label1.Text = "农历节日：" + cc.CalendarHoliday + "，公历节日：" + cc.DateHoliday + "，农历：" + cc.ChineseDateString + "，节气：" + cc.ChineseTwentyFourDay + "，公历日期：" + cc.Date + "，是否闰年：" + cc.IsChineseLeapYear + "，" + cc.WeekDayHoliday;
+                
+        }
+
+        private void BindData(string searchtype) {
+            if (!string.IsNullOrEmpty(textBox1.Text.Trim())) {
+                IEnumerable<string> files = Directory.EnumerateFiles(textBox1.Text, searchtype, SearchOption.TopDirectoryOnly);
+                List<filemodel> myfile = new List<filemodel>();
+                filemodel myfilemodel = new filemodel();
+                foreach (string model in files)
+                {
+                    myfilemodel = new filemodel();
+                    myfilemodel.path = model;
+                    myfile.Add(myfilemodel);
+                }
+                GridViewFileShow.DataSource = myfile;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+            this.TransparencyKey = BackColor;
+            this.Opacity = 1;
+            this.comboBox1.Text = "*.aspx";
+            this.comboBox1.Items.AddRange(new object[] {"*.aspx","*.html"});
+            //this.comboBox1.SelectionStart = 1;
+        }
+        //设置窗口透明度
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.Opacity = Math.Round((Double)(12-trackBar1.Value) / (Double)10, 1);
+        }
+        
+        //搜索框内容改变时
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text != null) {
+                BindData(comboBox1.Text);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //分析文件
+            DataGridViewCheckBoxCell check = new DataGridViewCheckBoxCell();
+            List<string> pathlist = new List<string>();
+            foreach (DataGridViewRow row in GridViewFileShow.Rows) {
+                check = (DataGridViewCheckBoxCell)row.Cells[0];
+                if ((bool)check.EditedFormattedValue || (bool)check.FormattedValue) {
+                    pathlist.Add(row.Cells[1].Value.ToString());
+                }
+            }
+            progressBar1.Maximum = pathlist.Count;
+            progressBar1.Step = 1;
+            foreach(string path in pathlist){
+                GetFileCssJSLink(path,"js");
+                progressBar1.PerformStep();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //处理文件    需要的参数   文件路径、正则类型
+            StringBuilder strhtml = ReadHtmlFile("");
+            Regex reg = new Regex("(src=\")(.*\\.js.*)(\")");
+            MatchCollection matches = reg.Matches(strhtml.ToString(), 0);
+            foreach (Match math in matches)
+            {
+                if (math.Groups[2].Value.IndexOf('?') > 0)
+                {
+                    string old = math.Groups[2].Value;
+                    old = old.Substring(0, math.Groups[2].Value.IndexOf('?'));
+                    strhtml.Replace(math.Groups[2].Value, old + "?v=" + DateTime.Now.Ticks);
+                }
+                else
+                {
+                    strhtml.Replace(math.Groups[2].Value, math.Groups[2].Value + "?v=" + DateTime.Now.Ticks);
+                }
+            }
+            
+            //WriteHtmlFile(strhtml, "D:\\发布web\\editlink\\" + path.Split('\\').Last());
+        }
+        
+        /// <summary>
+        /// 获取文件的链接  文件中所有的链接
+        /// </summary>
+        /// <param name="path">文件路径</param>
+        /// <param name="type">链接类型</param>
+        /// <returns></returns>
+        private List<string> GetFileCssJSLink(string path,string type)
+        {
+            List<string> result = new List<string>();
+            StringBuilder strhtml = ReadHtmlFile(path);
+            string regx = string.Empty;
+            if (type == "js"){
+                regx = "(src=\")(.*\\.js.*)(\")";
+                Regex reg = new Regex(regx);
+                MatchCollection matches = reg.Matches(strhtml.ToString(), 0);
+                foreach (Match math in matches)
+                {
+                    result.Add(math.Groups[2].Value);
+                }
+            }
+            else if (type == "css"){
+                regx = "(href=\")(.*\\.css.*)(\")";
+                Regex reg = new Regex(regx);
+                MatchCollection matches = reg.Matches(strhtml.ToString(), 0);
+                foreach (Match math in matches)
+                {
+                    result.Add(math.Groups[2].Value);
+                }
+            }
+            else {
+                regx = "((href=\")|(src=\"))((.*\\.css.*)|(.*\\.js.*))(\")";
+                Regex reg = new Regex(regx);
+                MatchCollection matches = reg.Matches(strhtml.ToString(), 0);
+                foreach (Match math in matches)
+                {
+                    result.Add(math.Groups[4].Value);
+                }
+            }
+            return result;
+        }
+
+        
+
+        private void historyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //菜单历史记录  需要的参数   文件路径、正则类型、替换的链接
+            MessageBox.Show("暂未完成该功能！");
+        }
+
+        /// <summary>
+        /// 读取文件
+        /// </summary>
+        /// <param name="temp"></param>Server.MapPath(".") + "/location_new.html")
+        /// <returns></returns>
+        public StringBuilder ReadHtmlFile(string temppath)
+        {
+            StringBuilder temp = new StringBuilder();
+            try
+            {
+                using (StreamReader sr = new StreamReader(temppath))
+                {
+                    temp.Append(sr.ReadToEnd());
+                    sr.Dispose();
+                    sr.Close();
+                }
+            }
+            catch (Exception exp)
+            {
+
+            }
+            return temp;
+        }
+
+        /// <summary>
+        /// 写入HTML文件
+        /// </summary>"F:/LIEZHONG2.0/LIEZHONGV2.Web/Common/test.htm"
+        /// <param name="str">HTML代码</param>
+        /// <param name="htmlfilename">完整带路径的文件名</param>
+        /// <returns></returns>
+        public bool WriteHtmlFile(StringBuilder str, string htmlfilename)
+        {
+            // 写文件 
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(htmlfilename, false, System.Text.Encoding.GetEncoding("UTF-8"))) //保存地址
+                {
+                    sw.WriteLine(str);
+                    sw.Flush();
+                    sw.Dispose();
+                    sw.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return true;
+        }
+    }
+}
